1e73b84a8346dedcd978d9942d2136b0fb5a3e4e
lohitha <nlohitha97@gmail.com>
2026-08-05 12:19:28 +0530

    check +,- or zero

---
+++ Day1/check.py
@@ -0,0 +1,8 @@
+# Check whether a number is positive, negative, or zero.
+n=int(input("enter n: "))
+if n>0:
+    print("Number is Positive")
+elif n<0:
+    print("Number is Negative")
+else:
+    print("Zero")
